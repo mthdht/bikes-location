570cf8b720ec80b247aab07811e3189845dd856e
mthdht <mthdht@gmail.com>
2018-06-10 23:43:22 +0200

    made mapManager skeleton

---
+++ js/mapManager.js
@@ -0,0 +1,34 @@
+/*
+ * Map manager for map handling
+ * Author: mthdht
+ * Version: 1.0
+ */
+
+function MapManager(map, stations) {
+    this.map = map;
+    this.stations = stations;
+
+    this.markers = [];
+
+    // this.init();
+}
+
+// TODO: create makeMarkers method
+MapManager.prototype.makeMarkers = function () {
+
+};
+
+// TODO: create showStationInfos method
+MapManager.prototype.showStationInfos = function (station) {
+
+};
+
+// TODO: create markerEventsListeners method
+MapManager.prototype.markersEventsListeners = function () {
+
+};
+
+// TODO: create init method
+MapManager.prototype.init = function () {
+
+};
